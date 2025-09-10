ab8da8851d4f02b8d80c941787ec6b33b329243b
Matheus <matheusglautert@gmail.com>
2025-09-10 20:12:15 -0300

    adicionado att proj01

--- projeto01/app/src/main/java/ads/mov/MainActivity.kt
+++ projeto01/app/src/main/java/ads/mov/MainActivity.kt
@@ -12,6 +12,18 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import ads.mov.ui.theme.MyApplicationTheme
+import android.content.res.Configuration
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.statusBars
+import androidx.compose.foundation.layout.windowInsetsPadding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -32,16 +44,43 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+    Surface(color = MaterialTheme.colorScheme.primaryContainer, modifier = modifier
+        .fillMaxWidth()
+        .windowInsetsPadding(
+            WindowInsets.statusBars
+        )) {
+        Column {
+            Text(
+                text = "Hello $name!",
+                fontSize = 16.sp,
+                fontStyle = FontStyle.Italic,
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier.padding(all = 24.dp)
+            )
+
+            Text(
+                text = "Mais informações!",
+                fontSize = 16.sp,
+                fontStyle = FontStyle.Italic,
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier.padding(all = 24.dp)
+            )
+        }
+    }
 }
 
-@Preview(showBackground = true)
+@Preview
 @Composable
-fun GreetingPreview() {
+private fun GreetingPreview() {
     MyApplicationTheme {
-        Greeting("Android")
+        Greeting("ADS")
+    }
+}
+
+@Preview(showBackground = true, showSystemUi = true, uiMode = Configuration.UI_MODE_NIGHT_YES)
+@Composable
+private fun GreetingPreviewDark() {
+    MyApplicationTheme {
+        Greeting("ADS")
     }
 }
